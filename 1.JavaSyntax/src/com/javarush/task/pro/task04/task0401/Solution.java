5be6b4dd4138df8e64f0fcfb751193eaea61c620
Max922991 <max43223@gmail.com>
2022-02-13 21:40:41 +0300

    add solution for Java Syntax task 0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        int a1 = 0;
+        while (a1 != 100){
+            System.out.println(quote);
+            a1++;
+        }
+    }
+}
