d41e7fc7082b8e428678052a063ae87538a022a6
Max922991 <max43223@gmail.com>
2022-02-14 19:48:21 +0300

    add solution for Java Syntax task 0404

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -6,7 +6,15 @@ package com.javarush.task.pro.task04.task0404;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        int height = 0;
+        while (height < 5) {
+            int width = 0;
+            while (width < 10) {
+                System.out.print('Q');
+                width++;
+            }
+            System.out.println();
+            height++;
+        }
     }
 }
