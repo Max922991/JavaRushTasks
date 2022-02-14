ee4de425688a0f6c57cc03189d0c7afc357888e7
Max922991 <max43223@gmail.com>
2022-02-14 18:05:23 +0300

    add solution for Java Syntax task 0308

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0308/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task03.task0308;
+
+import java.util.Scanner;
+
+/* 
+Координатные четверти
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        
+    }
+}
